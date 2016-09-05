d2052476f5762d652ba38831f4b5c3572b770874
shootermv <shootermv@gmail.com>
2016-09-05 14:59:56 +0300

    top menus replaced by "search" & "groups"

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,11 +5,13 @@ import LoadingDots from './LoadingDots';
 const Header = ({loading}) => {
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      <IndexLink to="/" activeClassName="active">Groups</IndexLink>
       {" | "}
       <Link to="/courses" activeClassName="active">Questions</Link>
       {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+      <Link to="/about" activeClassName="active">
+        <span className="glyphicon glyphicon-search"> </span> 
+      </Link>
       {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
