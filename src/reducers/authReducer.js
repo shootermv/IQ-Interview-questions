aeb2dc30657783c5bff0d0b8d04cf3e38538303d
shootermv <shootermv@gmail.com>
2016-09-20 10:56:20 +0300

    adding auth panel component

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,22 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default function authReducer(state = initialState.auth, action) {
+  
+  switch (action.type) {
+    case types.LOGOUT:
+        return {
+            currently: types.ANONYMOUS,
+            username: "guest",
+            uid: null
+        };
+    case types.LOGIN_USER:
+        return {
+            currently: types.LOGIN_USER,
+            username: action.username,
+            uid: action.uid
+        };    
+    default:
+      return state;
+  }
+}
