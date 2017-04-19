8624c2fee10ff94667306050e52d0b946a961b99
shootermv <shootermv@gmail.com>
2017-04-19 13:15:09 +0300

    fix lint error

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './store/configureStore';
-import {Provider} from 'react-redux';
-import { Router, Route , browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import routes from './routes';
-import {startListeningToAuth}  from './actions/authActions';
+import { startListeningToAuth } from './actions/authActions';
 
 
 import '../node_modules/materialize-css/dist/css/materialize.css';
 import '../node_modules/materialize-css/dist/js/materialize.js';
-import './styles/styles.css'; 
+import './styles/styles.css';
 
 const store = configureStore();
 //auth:
@@ -20,8 +20,7 @@ store.dispatch(startListeningToAuth());
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}>
-    </Router>
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
