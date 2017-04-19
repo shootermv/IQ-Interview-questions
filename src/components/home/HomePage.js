586f62493df07cab2fd6450be7e81bae02278d74
shootermv <shootermv@gmail.com>
2017-04-19 16:39:01 +0300

    remove marked content

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,22 +1,22 @@
-import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as groupActions from '../../actions/groupActions';
 import GroupList from './GroupList';
 
-
+//more
 class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
 
   render() {
-    const {groups} = this.props;
+    const { groups } = this.props;
     return (
       <div className="jumbotron">
-       {groups.length>0 && <h1>Groups</h1>}
-        <GroupList groups={groups}/>
+        {groups.length > 0 && <h1>Groups</h1>}
+        <GroupList groups={groups} />
       </div>
     );
   }
@@ -39,5 +39,3 @@ HomePage.propTypes = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
-
-//export default HomePage;
